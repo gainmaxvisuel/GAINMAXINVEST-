38e10e5f5871e8ae72a155c826468c644de72ac0
gainmaxvisuel <gainmaxvisuel@gmail.com>
2026-03-06 17:06:24 +0000

    Update FixedMenu.jsx

--- pages/pages/pages/pages/pages/pages/pages/pages/pages/components/components/FixedMenu.jsx
+++ pages/pages/pages/pages/pages/pages/pages/pages/pages/components/components/FixedMenu.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+export default function FixedMenu() {
+  return (
+    <div className="fixed-menu">
+      <button>Accueil</button>
+      <button>Mes VIP</button>
+      <button>Équipe</button>
+      <button>Profil</button>
+    </div>
+  );
+}
